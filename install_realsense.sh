f6843599f78f7390ce868362f6f0c32cc449b18b
Sangil Lee <aeternuslsi@gmail.com>
2016-11-26 18:55:58 +0900

    modify export address

--- install_realsense.sh
+++ install_realsense.sh
@@ -1,9 +1,9 @@
-cd $(WORKSPACE)/src
+cd $WORKSPACE/src
 git clone https://github.com/intel-ros/realsense.git
 git clone https://github.com/IntelRealSense/librealsense.git
 git clone https://github.com/ktossell/libuvc.git
 
 sudo apt-get install libv4l-dev
 
-cd $(WORKSPACE)
+cd $WORKSPACE
 catkin_make
